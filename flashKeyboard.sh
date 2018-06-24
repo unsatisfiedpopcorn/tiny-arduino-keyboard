b295ac45f789227fd321ebb44f3db99267f7e099
circularhorses <jefferykwoh@gmail.com>
2018-06-24 16:18:43 +0800

    Add dfu-programmer start to script. No longer need to power cycle arduino after flashing

--- flashKeyboard.sh
+++ flashKeyboard.sh
@@ -4,4 +4,6 @@ echo "Erased"
 sudo dfu-programmer atmega16u2 flash firmware/Arduino-keyboard-0.3.hex
 echo "Flashed"
 sudo dfu-programmer atmega16u2 reset
-echo "Resetted, script done!"
+echo "Resetted"
+sudo dfu-programmer atmega16u2 start
+echo "Ardunio started, script done!"
--- flashUsbserial.sh
+++ flashUsbserial.sh
@@ -4,4 +4,6 @@ echo "Erased"
 sudo dfu-programmer atmega16u2 flash firmware/Arduino-usbserial-uno.hex
 echo "Flashed"
 sudo dfu-programmer atmega16u2 reset
-echo "Resetted, script done!"
+echo "Resetted"
+sudo dfu-programmer atmega16u2 start
+echo "Ardunio started, script done!"
