04f5563312765a8f934fabbb03b0d7b5c4d8efee
circularhorses <jefferykwoh@gmail.com>
2018-07-28 00:05:37 +0800

    Better expression of control flow

--- macroKeys/macroKeys/ViewController.swift
+++ macroKeys/macroKeys/ViewController.swift
@@ -20,13 +20,11 @@ class ViewController: NSViewController {
     lazy var keyButtonCollection = [keyButton1, keyButton2, keyButton3, keyButton4]
     
     @IBAction func keyButton(_ sender: NSButton) {
-        // TODO Clear dictionary ONLY if there is a change detected
         if sender.state == NSButton.StateValue.on {
             // Ensures that only one keyButton can be "on" at a time.
             for keyButton in keyButtonCollection.filter({$0 != sender}) {
-                keyButton?.state = NSButton.StateValue.off
+                onCommitDeactivate(button: keyButton!)
             }
-            
             print("on")
             
         } else if sender.state == NSButton.StateValue.off {
@@ -35,8 +33,9 @@ class ViewController: NSViewController {
         updateView()
     }
     
-    func deactivateButton(_ sender: NSButton) {
-        
+    func onCommitDeactivate(button sender: NSButton) {
+        sender.state = NSButton.StateValue.off
+        keyDict.commit(onButton: sender)
     }
     
     override func viewDidLoad() {
@@ -48,21 +47,14 @@ class ViewController: NSViewController {
             return $0
         }
         NSEvent.addLocalMonitorForEvents(matching: .keyDown) {
-            if $0.keyCode == 53 { // ESC is pressed, then rollback currently recorded keys
-                for keyButton in self.keyButtonCollection {
-                    if keyButton?.state == NSButton.StateValue.on {
-                        keyButton?.state = NSButton.StateValue.off
-                        self.keyDict.rollback(onButton: keyButton!)
-                    }
-                }
-            } else {
-                for keyButton in self.keyButtonCollection {
-                    if keyButton?.state == NSButton.StateValue.on {
-                        print($0.characters!)
-                        // Update Dictionary of key mappings
-                        self.keyDict.addKey(loggedBy: keyButton!, loggedKey: $0)
-                        // Update view
-                    }
+            for keyButton in self.keyButtonCollection.filter({$0!.state == NSButton.StateValue.on}) {
+                if $0.keyCode == 53 { // ESC is pressed, then rollback currently recorded keys
+                    keyButton?.state = NSButton.StateValue.off
+                    self.keyDict.rollback(onButton: keyButton!)
+                } else {
+                    print($0.characters!)
+                    // Update Dictionary of key mappings
+                    self.keyDict.addKey(loggedBy: keyButton!, loggedKey: $0)
                 }
             }
             self.updateView()
@@ -71,6 +63,7 @@ class ViewController: NSViewController {
             return $0
         }
     }
+    
     
     func updateView() {
         for keyButton in keyButtonCollection {
