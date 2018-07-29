91e4fc35f760fca912729fe1359ccdda6a4d4630
circularhorses <jefferykwoh@gmail.com>
2018-07-29 18:20:47 +0800

    Add comments for Key class

--- macroKeys/macroKeys/lib/keyboardData/Key.swift
+++ macroKeys/macroKeys/lib/keyboardData/Key.swift
@@ -5,6 +5,8 @@
 //  Created by Chelsea Chia on 22/7/18.
 //  Copyright © 2018 Chelsea Chia. All rights reserved.
 //
+
+// TODO: let characters be a computed variable based on the keycode (ignoring all modifiers including Shift)
 import Foundation
 import Cocoa
 
@@ -20,14 +22,19 @@ struct Key : Codable, Equatable {
         return lhs.keycode == rhs.keycode
     }
     
+    /**
+     Initialises with a CGKeycode and a given char
+     */
     init(keycode: CGKeyCode, char: String) {
         self.keycode = keycode
         self.characters = char
     }
     
+    /**
+     Initialises with an NSEvent, extracting all the required keycode and characters
+     */
     init(withEvent keyEvent: NSEvent) {
         self.keycode = keyEvent.keyCode
         self.characters = keyEvent.charactersIgnoringModifiers
     }
-    
 }
