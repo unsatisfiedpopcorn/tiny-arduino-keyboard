85e0143bd43a5bbb3787580817fb110bff685fde
circularhorses <jefferykwoh@gmail.com>
2018-07-29 18:17:06 +0800

    Add comments for Modifier class

--- macroKeys/macroKeys/lib/keyboardData/Modifier.swift
+++ macroKeys/macroKeys/lib/keyboardData/Modifier.swift
@@ -8,8 +8,10 @@
 import Foundation
 import Cocoa
 
+// TODO: Throw error when non-Modifier bitmasks are initialised
 struct Modifier : Codable, Equatable {
-    var char: String {
+    
+    var char: String { // Computed variable that is dependent on the bitmask
         get {
             let sanitizedMask = self.rawMask
             var charactersArray = [String]()
@@ -37,53 +39,51 @@ struct Modifier : Codable, Equatable {
             return charactersArray.joined(separator: "+")
         }
     }
-    var mask: CGEventFlags {
+    
+    var mask: CGEventFlags { // Convenience variable that converts the rawMask to a CGEventFlags
         get {
             return CGEventFlags.init(rawValue: self.rawMask)
         }
     }
-    let rawMask: UInt64
     
+    let rawMask: UInt64 // rawMask in binary corresponding to keyboard's modifier's mask
+    
+    /**
+     Overrides equality. Two Modifiers are equal as long as they have the same rawMast / bitmask
+    */
     static func == (lhs: Modifier, rhs: Modifier) -> Bool {
         return lhs.rawMask == rhs.rawMask
     }
     
+    // Inits with an empty bit mask
     init() {
         self.rawMask = 0
     }
     
-    // Deprecated init
-//    init(char: String) {
-//        self.char = char
-//        if (char == "left_shift") {
-//            self.rawMask = CGEventFlags.maskShift.rawValue
-//        } else if (char == "left_control") {
-//            self.rawMask = CGEventFlags.maskControl.rawValue
-//        } else if (char == "left_option") {
-//            self.rawMask = CGEventFlags.maskAlternate.rawValue
-//        } else if (char == "left_gui") {
-//            self.rawMask = CGEventFlags.maskCommand.rawValue
-//        } else {
-//            self.rawMask = CGEventFlags(rawValue: 0).rawValue
-//        }
-//    }
-    
+    // Sanitizes the incoming bitmask and applies it
     init(withBitmask bitmask: UInt64) {
         let sanitizedMask = bitmask & UInt64(NSEvent.ModifierFlags.deviceIndependentFlagsMask.rawValue)
         self.rawMask = sanitizedMask
     }
     
+    // Convenience init that takes in a CGEventFlags
     init(withCGEventFlag flag: CGEventFlags) {
         self.init(withBitmask: flag.rawValue)
     }
     
+    /**
+     Can be chained
+     Applies a bitwise OR and returns the result
+    */
     func or(bitmask: UInt64) -> Modifier {
         return Modifier(withBitmask: self.rawMask | bitmask)
     }
     
+    /**
+     Can be chained
+     Applies a bitwise AND and returns the result
+     */
     func and(bitmask: UInt64) -> Modifier {
         return Modifier(withBitmask: self.rawMask & bitmask)
     }
-    
-    
 }
